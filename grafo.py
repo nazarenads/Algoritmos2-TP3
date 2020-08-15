797196402a28933a04ee067917441f657e02e9a1
Nazarena Dos Santos <nazarena.dossantos@intive.com>
2020-08-15 17:40:36 -0300

    fixed agregar arista

--- grafo.py
+++ grafo.py
@@ -1,20 +1,21 @@
 class Grafo:
-    def __init__(self):
+    def __init__(self, dirigido):
+        self.dirigido = dirigido
         self.vertices = {}
         self.cantidad_vertices = 0
 
     def agregar_vertice(self, vertice):
         if vertice not in self.vertices:
-            self.vertices[vertice] = []
+            self.vertices[vertice] = {}
             self.cantidad_vertices += 1
 
     def vertice_pertenece(self, vertice):
         return vertice in self.vertices
 
-    def agregar_arista(self, inicio, fin):
-        if not (self.vertice_pertenece(inicio) and self.vertice_pertenece(fin)):
+    def agregar_arista(self, v, w, peso=None):
+        if not (self.vertice_pertenece(v) and self.vertice_pertenece(w)):
             return False
-        self.vertices[inicio].append(fin)
+        self.vertices[v][w] = peso
+        if not self.dirigido:
+            self.vertices[w][v] = peso
         return True
-
-# si agrego vertices A y B el grafo queda: self.vertices = {"A": {}, "B":{}}
