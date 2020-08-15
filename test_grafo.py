7bf5c2fffcafe5a1c4da8779ecb459dde18fd870
Nazarena Dos Santos <nazarena.dossantos@intive.com>
2020-08-15 18:36:07 -0300

    added test for vertice_pertenece

--- test_grafo.py
+++ test_grafo.py
@@ -1,9 +1,15 @@
 import unittest
+from grafo import Grafo
 
 
 class TestGrafo(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)
+    def test_vertice_pertenece(self):
+        #creo un grafo no dirigido
+        grafo = Grafo(False)
+        #le agrego un vértice
+        grafo.agregar_vertice("A")
+        #me fijo si el vértice pertenece o no al grafo
+        self.assertEqual(grafo.vertice_pertenece("A"), True)
 
 
 if __name__ == '__main__':
