180c1c181020160f9b67b792632ec2cb3351d5fe
Erne <ernestodainesiperrot@gmail.com>
2020-08-15 20:08:28 -0300

    fixed BFS

--- grafo_utils.py
+++ grafo_utils.py
@@ -18,4 +18,6 @@ def bfs(grafo, origen, destino = None):
                 orden[w] = orden[v] + 1
                 visitados.add(w)
                 cola.append(w)
+        if destino != None and v == destino:
+            break
     return padre, orden
