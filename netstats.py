73c8abaa59ddc95c5314abca120c83c3ef958144
Nazarena Dos Santos <nazarena.dossantos@intive.com>
2020-08-16 14:37:02 -0300

    changes in netstats

--- netstats.py
+++ netstats.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 import os
 import sys
 
@@ -9,7 +10,8 @@ def cargar_datos_tsv(nombre_tsv):
         datos = []
         with open(nombre_tsv) as archivo:
             for linea in archivo:
-                dato = linea.split('\t')
+                linea_sin_espacios = linea.strip()
+                dato = linea_sin_espacios.split('\t')
                 datos.append(dato)
             return datos
     else:
@@ -20,18 +22,19 @@ def crear_red(datos):
     grafo = Grafo(True)
     crear_vertices(datos, grafo)
     crear_aristas(datos, grafo)
+    #print(grafo.ver_cantidad_vertices())
 
 
 def crear_vertices(datos, grafo):
     for dato in datos:
-        for i in range(len(dato)):
-            grafo.agregar_vertice(dato[i])
+        for articulo in dato:
+            grafo.agregar_vertice(articulo)
 
 
 def crear_aristas(datos, grafo):
     for dato in datos:
-        for i in range(1, len(dato)):
-            grafo.agregar_arista(dato[0], dato[i])
+        for articulo in dato[1:]:
+            grafo.agregar_arista(dato[0], articulo)
 
 
 def procesar_archivo(archivo_tsv):
