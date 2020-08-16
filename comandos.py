4833802e45ad2b94c2b9790f651183528ebc7085
Erne <ernestodainesiperrot@gmail.com>
2020-08-16 17:10:28 -0300

    added diametro

--- comandos.py
+++ comandos.py
@@ -11,9 +11,7 @@ def reconstruir_camino(padres, origen, destino):
     camino.reverse()
     return camino
 
-def camino_minimo(grafo, origen, destino):
-    padres, orden = bfs(grafo, origen, destino)
-    camino = reconstruir_camino(padres, origen, destino)
+def mostrar_camino(camino, destino, orden):
     resultado = ""
     for v in camino:
         if v != destino:
@@ -26,6 +24,27 @@ def camino_minimo(grafo, origen, destino):
         print(resultado)
         print(f"Costo: {orden[destino]}")
 
+def camino_minimo(grafo, origen, destino = None):
+    padres, orden = bfs(grafo, origen, destino)
+    camino = reconstruir_camino(padres, origen, destino)
+    mostrar_camino(camino, destino, orden)
+
+def diametro(grafo):
+    orden_max = {}
+    costo_max = 0
+    origen_max = ""
+    destino_max = ""
+    for v in grafo.obtener_vertices():
+        padres, orden = bfs(grafo, v)
+        costo = max(orden.values())
+        if(costo > costo_max):
+            costo_max = costo
+            orden_max = orden
+            destino_max = max(orden, key=orden_max.get)
+            origen_max = v
+    camino_max = reconstruir_camino(padres, v, destino_max)
+    mostrar_camino(camino_max, destino_max, orden_max)
+
 def main():
     grafo = Grafo(True)
     grafo.agregar_vertice("A")
@@ -35,6 +54,9 @@ def main():
     grafo.agregar_arista("A", "C")
     grafo.agregar_arista("C", "B")
     grafo.agregar_arista("B", "D")
-    camino_minimo(grafo, "D", "A")
+    camino_minimo(grafo, "A", "D")
+    dic = {"A": 4, "B": 2}
+    max_dic = max(dic, key=dic.get)
+    print(max_dic)
 
 main()
