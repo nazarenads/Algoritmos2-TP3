41ac978e5e3ce331cb5d0a50cd9b797f32c4148a
Erne <ernestodainesiperrot@gmail.com>
2020-08-16 15:14:02 -0300

    fixed camino_minimo

--- comandos.py
+++ comandos.py
@@ -6,23 +6,26 @@ def reconstruir_camino(padres, origen, destino):
     v = destino
     while v != None:
         camino.append(v)
+        if v not in padres: break
         v = padres[v]
     camino.reverse()
     return camino
 
 def camino_minimo(grafo, origen, destino):
     padres, orden = bfs(grafo, origen, destino)
-    print(padres)
     camino = reconstruir_camino(padres, origen, destino)
-    print(f"Camino: {camino}")
     resultado = ""
     for v in camino:
         if v != destino:
             resultado += v + " -> "
         else:
             resultado += v
-    print(resultado)
-    print(f"Costo: {orden[destino]}")
+    if destino not in orden:
+        print(origen)
+        print("Costo: 0")
+    else:
+        print(resultado)
+        print(f"Costo: {orden[destino]}")
 
 def main():
     grafo = Grafo(True)
@@ -33,6 +36,6 @@ def main():
     grafo.agregar_arista("A", "C")
     grafo.agregar_arista("C", "B")
     grafo.agregar_arista("B", "D")
-    camino_minimo(grafo, "A", "B")
+    camino_minimo(grafo, "D", "A")
 
 main()
